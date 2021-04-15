58f7b4f32ed3668ac79a97ab9a2de1c2f157a76f
Roger A. Light <roger@atchoo.org>
2021-04-15 11:50:30 +0100

    Version is actually 2.3.1

--- snap/local/streamsheets/gateway.sh
+++ snap/local/streamsheets/gateway.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source $SNAP/streamsheets_env.sh
+. $SNAP/streamsheets_env.sh
 
 cd $SNAP_COMMON
 rm -f $SNAP_COMMON/gateway.sock
--- snap/local/streamsheets/service-machines.sh
+++ snap/local/streamsheets/service-machines.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source $SNAP/streamsheets_env.sh
+. $SNAP/streamsheets_env.sh
 
 cd $SNAP_COMMON
 $SNAP/wait-for-database.sh && $SNAP/usr/bin/node $SNAP/packages/service-machines/start.js 2>&1 > $SNAP_COMMON/service-machines.log
