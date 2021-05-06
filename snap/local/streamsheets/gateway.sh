d05d6228ea3b80bc79692ad2ff46c990583cec0e
Roger A. Light <roger@atchoo.org>
2021-05-06 13:42:54 +0100

    Update to modern node.

--- snap/local/streamsheets/gateway.sh
+++ snap/local/streamsheets/gateway.sh
@@ -4,4 +4,4 @@
 
 cd $SNAP_COMMON
 rm -f $SNAP_COMMON/gateway.sock
-$SNAP/wait-for-database.sh && $SNAP/usr/bin/node $SNAP/packages/gateway/out/start.js 2>&1 > $SNAP_COMMON/gateway.log
+$SNAP/wait-for-database.sh && $SNAP/bin/node $SNAP/packages/gateway/out/start.js 2>&1 > $SNAP_COMMON/gateway.log
--- snap/local/streamsheets/service-graphs.sh
+++ snap/local/streamsheets/service-graphs.sh
@@ -3,4 +3,4 @@
 . $SNAP/streamsheets_env.sh
 
 cd $SNAP_COMMON
-$SNAP/wait-for-database.sh && $SNAP/usr/bin/node $SNAP/packages/service-graphs/start.js 2>&1 > $SNAP_COMMON/service-graphs.log
+$SNAP/wait-for-database.sh && $SNAP/bin/node $SNAP/packages/service-graphs/start.js 2>&1 > $SNAP_COMMON/service-graphs.log
