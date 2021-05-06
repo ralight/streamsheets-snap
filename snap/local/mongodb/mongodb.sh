e3dd4f6c42134230fb0b02ef3fd726215e247159
Roger A. Light <roger@atchoo.org>
2021-05-06 11:32:56 +0100

    Disable mongodb diagnostics - prevents huge spill of denials in logs.

--- snap/local/mongodb/mongodb.sh
+++ snap/local/mongodb/mongodb.sh
@@ -5,4 +5,4 @@ CONFIG_FILE="$SNAP/mongodb.conf"
 mkdir -p $SNAP_COMMON/mongodb
 rm -f $SNAP_COMMON/mongodb.sock
 
-$SNAP/usr/bin/mongod -f $CONFIG_FILE
+$SNAP/usr/bin/mongod -f $CONFIG_FILE --setParameter diagnosticDataCollectionEnabled=false
